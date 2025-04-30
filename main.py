7dff60e98170d67ca9b70f90a3183d528d91dab6
Murzakulovss <samat3267@gmail.com>
2025-04-30 22:07:20 +0600

    refactor: redesign OOP, separate Player

--- main.py
+++ main.py
@@ -1,13 +1,11 @@
 import random
 from words_list import words
 
-class HangmanGame:
+class Game:
     def __init__(self):
         self.word = random.choice(words)
-        self.guessed_letters = []
-        self.mistakes = []
-        self.tries = 6
         self.masked_word = ""
+        self.player = Player()
 
     def new_game(self):
         start = input("[N]ew game? or [E]xit?:  ")
@@ -18,31 +16,16 @@ class HangmanGame:
             exit()
 
     def start_game(self):
-        while self.tries > 0:
-            self.guess(input("guess a letter: "))
+        while self.player.tries > 0:
+            letter = input("guess a letter: ")
+            self.player.guess(letter, self.word)
             self.display_word()
             self.check_win()
-
-    def guess(self, letter):
-        if len(letter) > 1:
-            print("only one letter")
-            return
-        elif letter in self.guessed_letters or letter in self.mistakes:
-            print("you already checked this letter")
-            return
-        elif letter in self.word:
-            self.guessed_letters.append(letter)
-            print("hit!")
-        else:
-            self.tries-=1
-            self.mistakes.append(letter)
-            print(self.mistakes)
-            print(f"{self.tries} tries")
 
     def display_word(self):
         self.masked_word = ""
         for letter in self.word:
-            if letter in self.guessed_letters:
+            if letter in self.player.guessed_letters:
                 self.masked_word+=letter
             else:
                 self.masked_word+="*"
@@ -57,32 +40,41 @@ class HangmanGame:
             self.check_loss()
 
     def check_loss(self):
-        if self.tries == 0:
+        if self.player.tries == 0:
             print(f"The word was: {self.word}")
             self.new_game()
             return
 
     def reset(self):
         self.word = random.choice(words)
-        self.guessed_letters = []
-        self.mistakes = []
-        self.tries = 6
+        self.player.guessed_letters = []
+        self.player.mistakes = []
+        self.player.tries = 6
         self.masked_word = ""
 
 
-h = HangmanGame()
-h.new_game()
+class Player:
+    def __init__(self):
+        self.guessed_letters = []
+        self.mistakes = []
+        self.tries = 6
+
+    def guess(self, letter, word):
+        if len(letter) > 1:
+            print("only one letter")
+            return
+        elif letter in self.guessed_letters or letter in self.mistakes:
+            print("you already checked this letter")
+            return
+        elif letter in word:
+            self.guessed_letters.append(letter)
+            print("hit!")
+        else:
+            self.tries-=1
+            self.mistakes.append(letter)
+            print(self.mistakes)
+            print(f"{self.tries} tries")
 
 
-
-
-
-
-
-
-
-
-
-
-
-
+g = Game()
+g.new_game()
